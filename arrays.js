0c85cefc5f80bd70552adaa7f1c2f6980ae40213
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-21 14:37:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,10 @@ function accessElementInArray(array,index){
   return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array){}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
 
 function removeElementFromBeginningOfArray(array){}
 
