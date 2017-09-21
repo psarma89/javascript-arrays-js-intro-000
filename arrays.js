377a989417b1251e402d8ba6b61beae8340b631d
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-21 14:24:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element)
