4a60ffae239317ccd6af6d8b8caa82f7f38467a6
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-21 14:18:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,7 @@ function addElementToBeginningOfArray(array, element){
 function destructivelyAddElementToBeginningOfArray(array,element)
 }
 
+function addElementToEndOfArray(array,element){}
+
+function destructivelyAddElementToEndOfArray(array,element){}
+
